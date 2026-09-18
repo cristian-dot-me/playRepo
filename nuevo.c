bc48057b2302fb3d92e45dec89fe800b19e97aac
cristian <crisstiannn1977@gmail.com>
2026-09-17 23:26:47 -0300

    Agregada super mega funcion de Victoria

--- nuevo.c
+++ nuevo.c
@@ -199,59 +199,6 @@ void mascara(int inputMatriz[][COLUMNAS]) {
 
 }
 
-#include <stdio.h>
-#include <stdbool.h>
-
-// recorrido horizontal para verificar victoria 	[ X ]
-// recorrido vertical para verificar victoria		[ X ]
-// recorrido en diagonal para verificar victoria	[...]
-
-void mostrarM(int m[][6], int filas, int columnas, bool pedantic);
-bool victoria(int m[][6], int orden);
-void remind(bool debug); 
-
-bool debug = true;
-
-int main(void) {
-
-	bool estado 	= false;
-	int m 		= 6;
-	int n 		= 6;
-	int pepe[6][6] = {
-
-		{0,0,0,0,0,0},
-		{1,2,1,2,1,0},
-		{1,2,2,0,0,0},
-		{1,0,0,0,0,0},
-		{1,0,0,0,0,0},
-		{0,0,0,1,1,1}
-
-	}; 
-
-
-	mostrarM(pepe, m, n, true);
-
-	if (victoria(pepe, m)) {
-		printf("Se cumple una condicion de victoria \n");
-	} else {
-		printf("No se cumple ninguna condicion de victoria \n");
-	}
-
-	remind(debug);
-
-	return 0;
-
-}
-
-void remind(bool debug) {
-	
-	if (debug) {
-		printf("[RECORDATORIO]: Borrar variable k y loop de debug en funcion victoria() \n");
-		printf("[RECORDATORIO]: Borrar variable ENTREGA_G \n");	
-	}
-
-}
-
 bool victoria(int m[][6], int orden) {
 
 	
@@ -391,11 +338,7 @@ bool victoria(int m[][6], int orden) {
 
 	c1 	= 0;
 	c2 	= 0;
-//	control = orden;
-//	inicio 	= 2;
-//	final 	= 3;
-	bool flag = true;
-/*
+
 	while(flag) {
 
 		for (i=inicio;i<control;i++) {
@@ -408,53 +351,8 @@ bool victoria(int m[][6], int orden) {
 
 		}
 	}
-*/
+
 	return victoria;
 
 }
-
-void mostrarM(int m[][6], int filas, int columnas, bool pedantic) {
-
-	int i 		= 0;
-	int j 		= 0;
-
-	if (filas > 6 || columnas > 6) {
-		printf("Fuera de limites en funcion mostrarM \n Revisar indices \n");
-		return;
-	} 
-
-	if (pedantic) {
-		printf("┏========================================┓ \n");
-	}
-
-	for (i=0;i<filas;i++) {
-
-		if (pedantic) {
-			printf("|");
-		}
-
-		for (j=0;j<columnas;j++) {
-
-			printf("%d", m[i][j]);
-
-			if (j < columnas - 1) {
-				printf("\t");
-			}
-
-		}
-
-		if (pedantic) {
-			printf("|");
-		}
-
-		printf("\n");
-	}
-
-	if (pedantic) {
-		printf("┗========================================┛ \n");
-	}
-
-
-}
-
 
